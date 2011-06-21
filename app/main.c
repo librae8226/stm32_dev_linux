ddb7d41a81432b99eb979d4c04110c0431bb7211
Librae <librae8226@gmail.com>
2011-06-21 23:44:15 +0800

    Nightly Build - Updated with libmaple.

--- app/main.c
+++ app/main.c
@@ -13,12 +13,18 @@ int main(void)
 {
 	int8 ch = 0;
 	uint8 i;
+	uint32 cnt1 = 0;
+	uint32 cnt2 = 0;
 		
 	system_init();
 	printf("Hello, leach ~\r\n");
 
 	while (1) {
-		gpio_toggle_bit(GPIOA, 1);
+		if (cnt1++ % 1000 == 0) {	
+			if (cnt2++ % 1000 == 0) {
+				gpio_toggle_bit(GPIOA, 1);
+			}
+		}
 #ifdef SERIAL_ECHO
 		if (USARTx->flag_trigger) {
 			for (i = 0; i < USARTx->cnt_trigger; i++) {
